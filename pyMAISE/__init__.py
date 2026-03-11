bb8d74868e3d473cf627ab0c3f0d7ce521ca8ffa
Leo Tunkle <tunkleo@umich.edu>
2026-03-10 21:41:11 -0400

    Bumpy version to 2.0.1

--- pyMAISE/__init__.py
+++ pyMAISE/__init__.py
@@ -20,4 +20,4 @@ from pyMAISE.utils import Boolean, Choice, Fixed, Float, Int, _try_clear
 _try_clear()
 
 # This should always be the last line of this file
-__version__ = "2.0.0"
+__version__ = "2.0.1"
